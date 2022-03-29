caf7fb0f24caa18d7ab11726837f83e61bc8410e
Shafi Sourov <sourov.cse.brur@gmail.com>
2022-03-29 23:10:16 +0900

    data laoder & model  loader

---
+++ model_loader/model.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+def build_fc_model():
+    fc_model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation=tf.nn.relu),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    return fc_model
+model = build_fc_model()
